bf4b519eb2f508eb72ba0d28ee151c97e4ed57ce
Maxence VALIERE <maxence.valiere@gmail.com>
2018-12-03 17:46:09 +0100

    ANDROID STUDIO CHASHED jidfigsdf

--- app/src/main/java/fr/epita/tictactoe/GameTable.kt
+++ app/src/main/java/fr/epita/tictactoe/GameTable.kt
@@ -37,91 +37,109 @@ class GameTable : AppCompatActivity(), View.OnClickListener {
             when (v.id) {
                 R.id.button1 -> {
                     if (playerPlaying == 1) {
-                        button1.text = "X"
+                        if (button1.text == "")
+                            button1.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button1.text = "0"
+                        if (button1.text == "")
+                            button1.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button2 -> {
                     if (playerPlaying == 1) {
-                        button2.text = "X"
+                        if (button2.text == "")
+                            button2.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button2.text = "0"
+                        if (button2.text == "")
+                            button2.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button3 -> {
                     if (playerPlaying == 1) {
-                        button3.text = "X"
+                        if (button3.text == "")
+                            button3.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button3.text = "0"
+                        if (button3.text == "")
+                            button3.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button4 -> {
                     if (playerPlaying == 1) {
-                        button4.text = "X"
+                        if (button4.text == "")
+                            button4.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button4.text = "0"
+                        if (button4.text == "")
+                            button4.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button5 -> {
                     if (playerPlaying == 1) {
-                        button5.text = "X"
+                        if (button5.text == "")
+                            button5.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button5.text = "0"
+                        if (button5.text == "")
+                            button5.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button6 -> {
                     if (playerPlaying == 1) {
-                        button6.text = "X"
+                        if (button6.text == "")
+                            button6.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button6.text = "0"
+                        if (button6.text == "")
+                            button6.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button7 -> {
                     if (playerPlaying == 1) {
-                        button7.text = "X"
+                        if (button7.text == "")
+                            button7.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button7.text = "0"
+                        if (button7.text == "")
+                            button7.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button8 -> {
                     if (playerPlaying == 1) {
-                        button8.text = "X"
+                        if (button8.text == "")
+                            button8.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button8.text = "0"
+                        if (button8.text == "")
+                            button8.text = "0"
                         playerPlaying = 1
                     }
                 }
                 R.id.button9 -> {
                     if (playerPlaying == 1) {
-                        button9.text = "X"
+                        if (button9.text == "")
+                            button9.text = "X"
                         playerPlaying = 2
                     }
                     else {
-                        button9.text = "0"
+                        if (button9.text == "")
+                            button9.text = "0"
                         playerPlaying = 1
                     }
                 }
